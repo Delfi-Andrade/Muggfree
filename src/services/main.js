2bb991bfe73b404d1ff9ecfd225d239891ee1e21
Delfi Andrade <emiandrade877@gmail.com>
2025-07-01 18:06:03 -0300

    Nuevos estilos

--- src/services/main.js
+++ src/services/main.js
@@ -2,10 +2,10 @@ const BASEAPI = "https://potterapi-fedeperin.vercel.app/es";
 
 export async function fetchData(tipo) {
   try {
-    const resp=await fetch(`${BASEAPI}/${tipo}`);
-    if(!resp.ok)throw new Error("No se pudo con la info pa");
-    const dato=await resp.json();
-    console.log(dato);
+    const resp = await fetch(`${BASEAPI}/${tipo}`);
+    if (!resp.ok) throw new Error("No se pudo con la info pa");
+    const dato = await resp.json();
+    return console.log(dato);
   } catch (e) {
     return console.error(e.message);
   }
