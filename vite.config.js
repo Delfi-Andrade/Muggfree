943b65b8a14e87aa7b620e844ce21a8f3ca31a42
Delfi Andrade <emiandrade877@gmail.com>
2025-07-03 18:58:43 -0300

    Solucionador de problemas

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        info: resolve(__dirname, "info.html"),
+      },
+    },
+  },
+});
